d44329f14a53931d69ba764d9eee6eb431294542
greatSumini <greatsumini@gmail.com>
2020-11-25 00:55:14 +0900

    chore: :fire: remove unused codes

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -4,9 +4,6 @@ import LoggerInstance from './logger';
 export default () => {
   try {
     Container.set('logger', LoggerInstance);
-
-    LoggerInstance.info('✌️ Agenda injected into container');
-
     return {};
   } catch (e) {
     LoggerInstance.error('🔥 Error on dependency injector loader: %o', e);
