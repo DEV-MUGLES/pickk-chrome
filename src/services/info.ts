9c658861577f5c459271a15222ca7ab02490474f
greatSumini <greatsumini@gmail.com>
2020-11-25 02:06:52 +0900

    feat(info): :sparkles: add InfoCrawlService

---
+++ src/services/info.ts
@@ -0,0 +1,46 @@
+import { Service, Inject } from 'typedi';
+
+import { parseHostName, correct, parseAll } from '../lib';
+import { InfoCrawlResult } from '../types';
+
+import * as infoCrawlers from '../crawlers/info';
+
+@Service()
+export default class InfoCrawlService {
+  constructor(@Inject('pool') private pool, @Inject('logger') private logger) {}
+
+  public async Crawl(_url: string): Promise<InfoCrawlResult> {
+    try {
+      const url = encodeURI(_url);
+      const host = this.getHost(url);
+      const crawler = this.getCrawler(host);
+
+      const result = await this.pool.process(
+        async (page, data) => {
+          // Navigate to given Url and wait until Angular is ready
+          // if it's an angular page.
+          await page.goto(data.url, {
+            waitUntil: 'networkidle0',
+          });
+          const result = await page.evaluate(data.crawler);
+          return result;
+        },
+        { url, crawler }
+      );
+
+      return correct(parseAll(result));
+    } catch (e) {
+      this.logger.error(e);
+      throw e;
+    }
+  }
+
+  private getHost = (url: string): string => {
+    return parseHostName(new URL(url).hostname);
+  };
+
+  private getCrawler = (host: string) => {
+    const name = '_' + host.replace(/\.|-/g, '');
+    return infoCrawlers[name];
+  };
+}
