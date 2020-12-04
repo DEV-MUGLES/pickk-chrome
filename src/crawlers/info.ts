710994611f084d5d430a272a055629283d573329
greatSumini <greatsumini@gmail.com>
2020-12-04 11:48:28 +0900

    refactor: :recycle: refactor hiphoper image crawl logic

--- src/crawlers/info.ts
+++ src/crawlers/info.ts
@@ -262,10 +262,8 @@ export const _hiphopercom = () => {
   const brandKor = (document.querySelector('#getBrandName') as HTMLDivElement)
     .textContent;
   const imageUrl = document
-    .querySelector('.thumbnails a')
-    .innerHTML.split('alt')[0]
-    .split('src=')[1]
-    .split('"')[1];
+    .querySelector('.thumbnails a > img')
+    .getAttribute('src');
   const salePrice =
     document.querySelector('dd.price_txt > strong')?.textContent || '0';
   const originalPrice =
